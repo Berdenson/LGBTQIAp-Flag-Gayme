d2f8f7ff9757076bd86f18893080ac72e5651a7a
ykh <braden.reighard@icloud.com>
2023-03-17 15:41:16 -0700

    added more files for flags

--- src/main/java/com/github/berdenson/lgbrqpflaggame/Flags.java
+++ src/main/java/com/github/berdenson/lgbrqpflaggame/Flags.java
@@ -9,6 +9,14 @@ public class Flags {
 
     public static void setFlags(){
         addFlag("A-spec","https://static.wikia.nocookie.net/lgbtqia-sandbox/images/d/d8/A-spec_main_flag.svg/revision/latest?cb=20220217141655");
+        addFlag("Abroromantic", "https://static.wikia.nocookie.net/lgbtqia-sandbox/images/9/9b/Abroromantic_flag.png/revision/latest/scale-to-width-down/1000?cb=20220220003743");
+        addFlag("Aceflux", "https://static.wikia.nocookie.net/lgbtqia-sandbox/images/8/8f/Aceflux.png/revision/latest?cb=20220218164553");
+        addFlag("Achillean", "https://static.wikia.nocookie.net/lgbtqia-sandbox/images/a/a9/Achillean_Flag.svg/revision/latest/scale-to-width-down/1000?cb=20220123044402");
+        addFlag("Aegoromantic", "https://static.wikia.nocookie.net/lgbtqia-sandbox/images/8/8f/Aegoromantic.png/revision/latest/scale-to-width-down/1000?cb=20221020134710");
+        addFlag("Aegosexual", "https://static.wikia.nocookie.net/lgbtqia-sandbox/images/8/86/Aegosexual.png/revision/latest?cb=20220206222719");
+        addFlag("Agender", "https://static.wikia.nocookie.net/lgbtqia-sandbox/images/6/68/Agender_4-stripe_Flag.svg/revision/latest/scale-to-width-down/1000?cb=20220123020041");
+        addFlag("Agender", "https://static.wikia.nocookie.net/lgbtqia-sandbox/images/0/04/Agender_Flag.svg/revision/latest/scale-to-width-down/1000?cb=20220123020312");
+        addFlag("Agender Spectrum", "https://static.wikia.nocookie.net/lgbtqia-sandbox/images/d/d6/Agender_Spectrum_Flag.png/revision/latest?cb=20220227015427");
 
     }
     private static void addFlag(String name, String url) {
